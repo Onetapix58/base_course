83f50fafa580d0e722cede5460e8e8923f4a25c4
Onetapix58 <rto23485@gmail.com>
2024-11-12 15:37:17 +0000

    ewrtetre

---
+++ ooo.py
@@ -0,0 +1,9 @@
+print ("Helo, World") 
+print (" Маша + Петя = Любовь ") 
+print ("x = 3 + 4")
+print (3 + 4)
+print ("x = 7" )
+a = [1, 5, "Good", "Bad"]
+b = [9, "Blue", "Red, 11"]
+print (a [1] + b [3])
+print (a [2] + b [2])
